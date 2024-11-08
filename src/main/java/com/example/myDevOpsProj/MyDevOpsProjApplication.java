4fb72962f6c7b25c26e1cedad1c2461e94e00eb0
Kamikoto-Mayer <danielmayer202@gmail.com>
2024-11-08 11:28:02 +0300

    Initial commit

---
+++ src/main/java/com/example/myDevOpsProj/MyDevOpsProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.myDevOpsProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDevOpsProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDevOpsProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myDevOpsProj/MyDevOpsProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myDevOpsProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDevOpsProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
